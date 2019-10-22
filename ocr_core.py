6c51fcdff7d37fa88daf9d7958a970bb8474fc27
miutamihai <miuta.mihai@gmail.com>
2019-10-22 13:19:59 +0100

    improved text recognition by enlarging the image

--- ocr_core.py
+++ ocr_core.py
@@ -8,7 +8,12 @@ def ocr_core(filename):
     """
     This function will handle the core OCR processing of images.
     """
-    text = pytesseract.image_to_string(Image.open(filename))  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
+    load_freq_dawg = False
+    load_system_dawg = False
+    img = Image.open(filename)
+    new_size = tuple(2*x for x in img.size)
+    img = img.resize(new_size, Image.ANTIALIAS)
+    text = pytesseract.image_to_string(img)  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
     return text
 
 print(ocr_core('images/test.jpg'))
